3b34ab1eb983b15590662ad85f8e2073f309880a
Shawn Wu <shawn.wu@sparkventures.co.nz>
2018-02-26 17:43:37 +1300

    Sample reduer

---
+++ js/redux/reducer/index.js
@@ -0,0 +1,26 @@
+//https://api.github.com/search/repositories?q=stars:%3E1&sort=stars
+export default () => {
+  return [
+    {
+      id: 28457823,
+      name: "freeCodeCamp",
+      html_url: "https://github.com/freeCodeCamp/freeCodeCamp",
+      open_issues: 188,
+      watchers: 291630
+    },
+    {
+        id: 28457824,
+        name: "bootstrap",
+        html_url: "https://github.com/freeCodeCamp/bootstrap",
+        open_issues: 18,
+        watchers: 291
+      },
+      {
+        id: 28457825,
+        name: "tensorflow",
+        html_url: "https://github.com/tensorflow/tensorflow",
+        open_issues: 1,
+        watchers: 2916
+      },
+  ];
+};
